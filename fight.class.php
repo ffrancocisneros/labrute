7790f795a1b562894aeaac34c8cc6781c7a09c83
Nadawoo <nadatruc@gmail.com>
2020-11-11 19:02:47 +0100

    Simplify the variables in the text strings

--- fight.class.php
+++ fight.class.php
@@ -21,20 +21,23 @@
 			
 			while ($attacker->Health > 0 && $attacked->Health > 0) {
 				$action = Random::num(0, 3);
+				$weapon_name = self::$Actions[$action][0];
 				
 				//The attacker hit on even and the attacked on odd
 				if (($hit % 2) != 0) {
-					$attacked->Health -= intval(self::$Actions[$action][1] + ($attacker->Strength * ($attacker->Strength / 100)));
+					$lost_lifepoints   = intval(self::$Actions[$action][1] + ($attacker->Strength * ($attacker->Strength / 100)));
+					$attacked->Health -= $lost_lifepoints;
 					
-					echo '"'.$attacker->Name.'" dio un/a "'.self::$Actions[$action][0].'" a "'.$attacked->Name.'" restandole '.intval(self::$Actions[$action][1] + ($attacker->Strength * ($attacker->Strength / 100))).' puntos de vida! (Le quedan '.$attacked->Health.' puntos de vida)'.'<br>';
+					echo '"'.$attacker->Name.'" dio un/a "'.$weapon_name.'" a "'.$attacked->Name.'" restandole '.$lost_lifepoints.' puntos de vida! (Le quedan '.$attacked->Health.' puntos de vida)'.'<br>';
 					
 					if ($attacked->Health <= 0) {
 						echo '"'.$attacker->Name.'" ha ganado la pelea!'.'<br>';
 					}
 				} else {
-					$attacker->Health -= intval(self::$Actions[$action][1] + ($attacked->Strength * ($attacked->Strength / 100)));
+					$lost_lifepoints   = intval(self::$Actions[$action][1] + ($attacked->Strength * ($attacked->Strength / 100)));
+					$attacker->Health -= $lost_lifepoints;
 					
-					echo '"'.$attacked->Name.'" dio un/a "'.self::$Actions[$action][0].'" a "'.$attacker->Name.'" restandole '.intval(self::$Actions[$action][1] + ($attacked->Strength * ($attacked->Strength / 100))).' puntos de vida! (Le quedan '.$attacker->Health.' puntos de vida)'.'<br>';
+					echo '"'.$attacked->Name.'" dio un/a "'.$weapon_name.'" a "'.$attacker->Name.'" restandole '.$lost_lifepoints.' puntos de vida! (Le quedan '.$attacker->Health.' puntos de vida)'.'<br>';
 					
 					if ($attacker->Health <= 0) {
 						echo '"'.$attacked->Name.'" ha ganado la pelea!'.'<br>';
